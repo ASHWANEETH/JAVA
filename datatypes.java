0cd5b22f2828e5b54cf5c2817103512ec4340c07
ASHWANEETH <ashwaneeth@gmail.com>
2024-07-09 17:09:19 +0530

    learning java

---
+++ datatypes.java
@@ -0,0 +1,18 @@
+public class datatypes {
+  public static void main(String[] args) {
+    int num = 20;
+
+    short sh = 10;
+    byte by = 10;  //-128 to 127
+    long l = 100l;
+
+    float f = 22.8f;
+    double d = 22.8;
+
+    char c = 'k'; // '' no ""
+
+    boolean b = true;
+    
+    System.out.println(num);
+  }
+}
