8cdaf64e6530eb48df91b3bfab50ad6f6e213f5b
unknown <ashwaneeth@gmail.com>
2025-12-09 12:50:14 +0530

    Datatypes

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Hello");
+
+        //Data types
+        //byte(1byte) short(2) int(4) long(8)
+        //float(4) double(8)
+        //boolean(1bit) char(2bytes)
+    }
+}
